de3ead9c006df4e6d46da82660eac66e40527fd0
BenDeBrasi <bdebrasi@gmail.com>
2018-03-06 12:06:44 -0500

    1.1 incomplete

--- CTCI/Arrays and Strings/isUnique.java
+++ CTCI/Arrays and Strings/isUnique.java
@@ -1,6 +1,18 @@
 public class isUnique{
-	public static void main(String[] args){
-		
 	
+	//Array for ascii value approach. Time: O(n) Space: 0(n)
+	static boolean isUnique1(String str){
+		int[] alphabet = new int[128];
+		for(int i = 0; i < str.length(); i++){
+			alphabet[str.charAt(i)] += 1;
+			if(alphabet[str.charAt(i)] > 1)
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args){
+		System.out.println(isUnique1("Helo"));
+		return;
 	}
 }
