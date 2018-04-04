d272a0f61266dafb18940f40b3b6eb658949335a
BenDeBrasi <bdebrasi@gmail.com>
2018-04-04 14:45:49 -0400

    Removed nums from isUnique.java

--- CTCI/Arrays and Strings/1.1isUnique.py
+++ CTCI/Arrays and Strings/1.1isUnique.py
@@ -1,11 +1,11 @@
 #Array ascii method no bit vector (original way I coded it).
-def isUnique1(str):
-	if len(str) > 128:
+def isUnique1(st):
+	if len(st) > 128:
 		return False
 		
 	list = [0] * 128
 	
-	for c in str:
+	for c in st:
 		list[ord(c)]+=1
 		if list[ord(c)] > 1:
 			return False
@@ -13,15 +13,16 @@ def isUnique1(str):
 	return True
 	
 #Sort method
-def isUnique2(str):
-	str.sort()
-	for i in xrange(1, len(str)):
-		if str[i] == str[i-1]:
+def isUnique2(st):
+	st = sorted(st)
+	for i in range(1, len(st)):
+		if st[i] == st[i-1]:
 			return False
 	return True
 	
 #Bitvector ascii method.
-def isUnique3(str):
-	if len(str) > 128:
+def isUnique3(st):
+	if len(st) > 128:
 		return False
-	
+
+print(isUnique2(""))	
