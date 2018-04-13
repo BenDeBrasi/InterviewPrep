985906f891aca349aaf3a0a4dc0c3630e0539efd
BenDeBrasi <bdebrasi@gmail.com>
2018-04-13 14:41:14 -0400

    1.1 work in Java and Python

--- CTCI/Arrays and Strings/isUnique.java
+++ CTCI/Arrays and Strings/isUnique.java
@@ -1,18 +1,32 @@
+import java.util.BitSet;
+import java.util.Arrays;
+
 public class isUnique{
 	
 	//Array for ascii value approach. Time: O(n) Space: 0(n)
 	static boolean isUnique1(String str){
-		int[] alphabet = new int[128];
+		BitSet alphabet = new BitSet(128);
 		for(int i = 0; i < str.length(); i++){
-			alphabet[str.charAt(i)] += 1;
-			if(alphabet[str.charAt(i)] > 1)
+			if(alphabet.get(str.charAt(i)) == true)
+				return false;
+			alphabet.flip(str.charAt(i));
+		}
+		return true;
+	}
+
+	//Sort String then compare neighboring letters.
+	static boolean isUnique2(String str){
+		char[] sorted = str.toCharArray();
+		Arrays.sort(sorted);
+		for(int i = 1; i < sorted.length; i++){
+			if(sorted[i] == sorted[i-1])
 				return false;
 		}
 		return true;
 	}
 
 	public static void main(String[] args){
-		System.out.println(isUnique1("Helo"));
+		System.out.println(isUnique2("Helo"));
 		return;
 	}
 }
