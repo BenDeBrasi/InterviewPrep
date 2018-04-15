0fe69d664fb330feaea4dc48530486536bad32f2
BenDeBrasi <bdebrasi@gmail.com>
2018-04-14 23:06:56 -0400

    1.3 working in Java and Python

--- CTCI/Arrays and Strings/1.3URLify.py
+++ CTCI/Arrays and Strings/1.3URLify.py
@@ -1,9 +1,34 @@
 def URLify(string):
-	list = ""
-	for c in string:
-		if c == ' ':
-			list.append("%20")
-		else:
-			list.append(c)
-	
-	return str(list)
+    st = ""
+    for c in string:
+        if c == ' ':
+            st += "%20"
+        else:
+            st += c
+    
+    return st
+
+def URLify1(string):
+    count = 0
+    for c in string:
+        if c == ' ':
+            count += 3
+        else:
+            count += 1
+    
+    arr = list(range(count))
+    
+    j = 0
+    for i in range(len(string)):
+        if string[i] == ' ':
+            arr[j] ='%'
+            arr[j+1] = '2'
+            arr[j+2] = '0'
+            j+=3
+        else:
+            arr[j] = string[i]
+            j+=1
+    
+    return ''.join(arr)
+
+print(URLify1("hello world"))
--- CTCI/Arrays and Strings/URLify.java
+++ CTCI/Arrays and Strings/URLify.java
@@ -1,28 +1,30 @@
 class URLify{
-	public static String URLify(string){
-		
+	public static String URLify(String string){
 		int space_count = 0;
 		
-		for(int i; i < string.length(); i++){
-			if(string.charAt(i).equals(' '){
-				space_count++;
-			}
+		for(int i= 0; i < string.length(); i++){
+			if(string.charAt(i) == ' ')
+				space_count++;	
 		}
 		
 		char[] URL_string = new char[(string.length()-space_count) + (space_count * 3)];
 		
 		int j = 0;
-		for(int i; i < string.length(); i++){
-			if(string.chatAt(i).equals(' ')){
+		for(int i = 0; i < string.length(); i++){
+			if(string.charAt(i) == ' '){
 				URL_string[j] = '%';
 				URL_string[j+1] = '2';
 				URL_string[j+2] = '0';
-				j +=3;
+				j+=2;
 			}
 			else
 				URL_string[j] = string.charAt(i);
 				j++;
 		}
-	return URL_string;
+		return new String(URL_string);
+	}
+
+	public static void main(String[] args){
+		System.out.println(URLify("hello world"));
 	}
 }
